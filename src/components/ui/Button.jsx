5a31bb9e7065c56c80f9664a4bf0e105fe2b660a
minhi0505 <mineehi0505@gmail.com>
2025-08-31 12:28:12 +0900

    5-2-2. 사용자 추가와 목록 렌더링

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Button.css';
+
+const Button = ({ children, type, className, onClick, disabled }) => {
+    const cn = `button ${className ?? ''}`;
+
+    return (
+        <button
+            type={type}
+            className={cn}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
